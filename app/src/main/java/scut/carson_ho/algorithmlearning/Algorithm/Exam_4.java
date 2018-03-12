d3e35bcfa124142292f757672f1c921cd2850cac
“Carson-Ho” <carson.ho@foxmail.com>
2018-03-12 19:45:06 +0800

    add

--- app/src/main/java/scut/carson_ho/algorithmlearning/Algorithm/Exam_4.java
+++ app/src/main/java/scut/carson_ho/algorithmlearning/Algorithm/Exam_4.java
@@ -48,16 +48,61 @@ public class Exam_4 {
     }
 
     /**
+     * 递归解法
+     * @param array = 二维数组
+     * @param target = 需查找的整数
+     * @return 返回false代表该数组无该整数；返回true代表该数组有该整数
+     */
+
+    private static boolean answer1(int[][] array,int target) {
+        // 1. 判断输入数据 是否合法
+        if (array == null) {
+            System.out.print("输入数据不合法");
+            return false;
+        }
+
+        return valueHelper(array,target,0,array[0].length-1);
+
+
+    }
+    // 递归辅助算法
+    private static boolean valueHelper(int[][] array,int target,int row,int col) {
+        // 1. 判断输入数据 是否合法
+        if (row >= array.length || col<0) {
+            System.out.print("遍历完毕");
+            return false;
+        }
+
+        if (target ==array[row][col]) return true;
+
+        if (target <array[row][col]){
+            col--;
+        }
+        else row++;
+
+        return valueHelper(array,target,row,col);
+
+    }
+
+    /**
      * 执行解题算法
      */
     public static void main(String[] args) {
 
         // 功能测试
         int[][] src = new int[][]{{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+
+        // 一般解法
         System.out.println(answer(src,7));
 
         // 特殊输入测试
         System.out.println(answer(null,7));
+
+        // 递归解法
+        System.out.println(answer1(src,7));
+
+        // 特殊输入测试
+        System.out.println(answer1(null,7));
 
     }
 
